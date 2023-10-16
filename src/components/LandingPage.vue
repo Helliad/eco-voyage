163eabfc22459fd1cb145cb8f9e1c2c6330f8570
Helliad <44154838+Helliad@users.noreply.github.com>
2023-10-16 15:28:19 +0800

    Initial commit

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="landingpage">
+        <div class="jumbotron position-relative">
+            <img src="..\..\src\assets\jumbotron_img.png" alt="Image description">
+            <div class="position-absolute top-50 translate-middle text-start" id="jumbo-text">
+                <h1 class="display-4">Discover Bali's Natural Wonders</h1>
+                <p class="lead">And Preserve Its Future</p>
+                <hr class="my-4">
+                <p>Feel free to explore and learn more about Vue.js.</p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.jumbotron {
+    background-color: #f8f9fa;
+}
+
+.jumbotron img {
+    width: 100%;
+}
+
+#jumbo-text{
+    margin-left:40%;
+    color:white;
+}
+
+
+</style>

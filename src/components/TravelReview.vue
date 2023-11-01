2450754d6de077433388b85f4b688d4ad75a48d7
Helliad <44154838+Helliad@users.noreply.github.com>
2023-11-01 19:45:03 +0800

    Travel Review responsive
    Stars are now displayed as well

--- src/components/TravelReview.vue
+++ src/components/TravelReview.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="travel-review mb-4">
         <h2>Travel Reviews</h2>
-        <div class="card-container col-lg-7 col-md-12 mx-auto">
+        <div class="card-group col-lg-7 col-md-12 mx-auto">
             <div class="card mx-3">
                 <div class="card-body">
                     <div class="row">
@@ -25,10 +25,10 @@
                             <img src="../assets/logo.png" alt="User Photo" class="user-photo">
                         </div>
                         <div class="col-9">
-                            <h5 class="card-title">{{ reviews[0].name }}</h5>
-                            <p class="card-text">{{ reviews[0].review }}</p>
+                            <h5 class="card-title">{{ reviews[1].name }}</h5>
+                            <p class="card-text">{{ reviews[1].review }}</p>
                             <div class="stars">
-                                <span v-for="(star, index) in reviews[0].stars" class="star" :key="index"></span>
+                                <span v-for="(star, index) in reviews[1].stars" class="star" :key="index"></span>
                             </div>
                         </div>
                     </div>
@@ -74,6 +74,7 @@ export default {
     width: 20px;
     height: 20px;
     background-color: gold;
+    clip-path: polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%);
     margin-right: 5px;
 }
 

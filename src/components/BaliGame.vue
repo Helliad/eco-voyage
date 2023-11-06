80f4feb153ca903e3fc76b93a4cac0871263cd32
Helliad <44154838+Helliad@users.noreply.github.com>
2023-11-06 23:25:04 +0800

    Pushed Pokemon game

---
+++ src/components/BaliGame.vue
@@ -0,0 +1,45 @@
+<template>
+  <div v-html="index"></div>
+  </template>
+  
+  <script>
+  import index from "../../pokemon-game/index.html"
+
+  export default {
+    data() {
+      return {
+        isNavbarCollapsed: false,
+      };
+    },
+    methods: {
+      toggleNavbarCollapse() {
+        this.isNavbarCollapsed = !this.isNavbarCollapsed;
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .navbar-brand {
+    font-weight: bold;
+    font-size: 1.5rem;
+  }
+  
+  .navbar-nav .nav-link {
+    font-size: 1.2rem;
+  }
+  
+  .navbar {
+    background-color: rgba(193, 227, 255);
+  }
+  
+  /* 
+  .navbar a{
+      color: rgb(225, 225, 225);
+  } */
+  
+  .navbar-nav {
+    margin-left: auto;
+  }
+  </style>
+  

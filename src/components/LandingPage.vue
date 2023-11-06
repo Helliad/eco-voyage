6ef69b022326b5dae2ffe76773eb9c27b5360b47
Helliad <44154838+Helliad@users.noreply.github.com>
2023-11-06 19:36:43 +0800

    Stylistic and Form Handling
    CheckOut date can not be before CheckIn date

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -6,7 +6,7 @@
                     <div class="text-start">
                         <h1 class="display-3 fw-bold mb-3">Discover Bali's Natural Wonders <u class="text-warning"><span
                                     class="text-success">Sustainably</span></u></h1>
-                        <p class="lead mb-4">Bali's booming tourism industry has negatively impacted the environment, from carbon footprints to plastic waste, and learn how you can plan your trip sustainably...
+                        <p class="lead mb-4">Bali's booming tourism industry has negatively impacted the environment, from carbon footprints to plastic waste, and learn how you can plan your trip sustainably.
                         </p>
                         <a href="#" class="btn btn-success btn mb-4">See Some Sustainable Itineraries</a>
                     </div>
